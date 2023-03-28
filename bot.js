df62d825711ec543537ac740ae4472f34bde79df
Vathna <c.neak@yahoo.com.au>
2023-03-28 20:49:22 +0700

    feat: basic response

--- bot.js
+++ bot.js
@@ -9,8 +9,14 @@ const openai = new OpenAIApi(config)
 
 const TelegramBot = require("node-telegram-bot-api")
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true})
-bot.on("message", (message, metadata) => {
-    const chatId = message.chat.id
-    response=""
-    bot.sendMessage(chatId, response)
+bot.on("message", (message) => {
+    openai.createChatCompletion({
+        model: process.env.OPENAI_MODEL,
+        messages: [
+            {role: "system", content: process.env.OPENAI_SYSTEM_MESSAGE},
+            {role: "user", content: message.text}
+        ]
+    }).then((completion) => {
+        bot.sendMessage(message.chat.id, completion.data.choices[0].message)
+    })
 })
