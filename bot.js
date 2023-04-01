3b718a5e1421750992447428ff2f8798f0dd83df
Vathna <c.neak@yahoo.com.au>
2023-04-01 16:33:30 +0700

    feat: read role

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 require("dotenv").config()
+const fs = require("fs")
 
 const Configuration = require("openai").Configuration
 const OpenAIApi = require("openai").OpenAIApi
@@ -11,13 +12,21 @@ const openai = new OpenAIApi(config)
 const TelegramBot = require("node-telegram-bot-api")
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true})
 bot.on("message", (message) => {
-    openai.createChatCompletion({
-        model: process.env.OPENAI_MODEL,
-        messages: [
-            {role: "system", content: process.env.OPENAI_SYSTEM_MESSAGE},
-            {role: "user", content: message.text}
-        ]
-    }).then((completion) => {
-        bot.sendMessage(message.chat.id, completion.data.choices[0].message.content)
+    const roleFilepath = "roles/" + message.chat.id
+    fs.readFile(roleFilepath, "utf-8", (error, data) => {
+        var role = process.env.OPENAI_DEFAULT_ROLE
+        if (error) console.log(error)
+        else role = data
+        
+        // openai.createChatCompletion({
+        //     model: process.env.OPENAI_MODEL,
+        //     messages: [
+        //         {role: "system", content: role},
+        //         {role: "user", content: message.text}
+        //     ]
+        // }).then((completion) => {
+        //     bot.sendMessage(message.chat.id, completion.data.choices[0].message.content)
+        // })
     })
+    
 })
